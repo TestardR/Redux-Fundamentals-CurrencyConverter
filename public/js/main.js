b8f7fd6f3472e3d92eadc20a7b11f5fef5b5d619
Romain <rmtestard@gmail.com>
2018-12-01 10:43:13 +0100

    react-redux

--- public/js/main.js
+++ public/js/main.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './stores/configureStore';
 import { Provider } from 'react-redux';
-import Conversion from './components/conversion.js';
+import Conversion from './containers/Conversion.js';
 
 class MainComponent extends React.Component {
-  // in order to rerender this component (and its children), we call setState once the component has rendered. Empty setState, trick to rerender
-  componentDidMount() {
-    store.subscribe(() => {
-      this.setState({});
-    });
-  }
   render() {
     return (
       <div>
-        <Conversion originAmount={store.getState().originAmount} />
+        <Conversion />
       </div>
     );
   }
--- public/js/stores/configureStore.js
+++ public/js/stores/configureStore.js
@@ -1,4 +1,6 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 // state
 var defaultState = {
@@ -7,7 +9,6 @@ var defaultState = {
 
 // reducer
 function amount(state = defaultState, action) {
-  console.log('state', state);
   if (action.type === 'CHANGE_ORIGIN_AMOUNT') {
     // dont mutate the state, instead use Object.assign
     // return Object.assign({}, state, { originAmount: action.data });
@@ -21,6 +22,6 @@ function amount(state = defaultState, action) {
 }
 
 // Reduxstore (dataStore where the state is saved)
-var store = createStore(amount);
+var store = createStore(amount, applyMiddleware(logger));
 
 export default store;
